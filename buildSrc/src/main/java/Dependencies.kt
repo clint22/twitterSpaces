eef6e7807c1f72604767990ad8a8217e349aef0f
clint <clint@bestdocapp.com>
2022-02-08 00:19:49 +0530

    initial commit

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,20 @@
+/**
+ * To define plugins
+ */
+object BuildPlugins {
+    val android by lazy { "com.android.tools.build:gradle:${Versions.gradle}" }
+    val kotlin by lazy { "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}" }
+}
+
+/**
+ * To define dependencies
+ */
+object Deps {
+    val appCompat by lazy { "androidx.appcompat:appcompat:${Versions.appCompat}" }
+    val ktx by lazy { "androidx.core:core-ktx:${Versions.ktx}" }
+    val materialDesign by lazy { "com.google.android.material:material:${Versions.material}" }
+    val constraintLayout by lazy { "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}" }
+    val junit by lazy { "junit:junit:${Versions.junit}" }
+    val junitTest by lazy { "androidx.test.ext:junit:${Versions.junitTest}" }
+    val espressoCore by lazy { "androidx.test.espresso:espresso-core:${Versions.espressoCore}" }
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,11 @@
+object Versions {
+    const val gradle = "7.0.1"
+    const val kotlin = "1.5.30-RC"
+    const val ktx = "1.7.0"
+    const val appCompat = "1.4.0"
+    const val material = "1.4.0"
+    const val constraintLayout = "2.1.2"
+    const val junit = "4.+"
+    const val junitTest = "1.1.3"
+    const val espressoCore = "3.4.0"
+}
